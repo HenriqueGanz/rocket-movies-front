693a5962946d7f08c171c6fd57149c943a3b534e
henriqueganz <henrique.ganz@gmail.com>
2023-06-02 16:00:31 -0300

    navbar without details complete

--- src/app/components/nav/index.js
+++ src/app/components/nav/index.js
@@ -5,8 +5,19 @@ export default function Nav() {
         <Container>
             <div id="itensNav">
                 <div id="divLogo"><span>RocketMovies</span></div>
-                <div id="divBusca">
+
+                <div id="divSearch">
                 <input type="text" id="txtBusca" placeholder="Buscar pelo título."/>
+                </div>
+
+                <div id="divProfile">
+                    <div id="profileButtons">
+                        <a href="#"><span class="username">Henrique Ganz</span></a>
+                        <a href="#"><span class="exitpage">Sair</span></a>
+                    </div>
+                    <div>
+                        <img id="imgProfile" src="http://github.com/henriqueganz.png" alt="Imagem de perfil do usuário"/>
+                    </div>
                 </div>
             </div>
         </Container>
--- src/app/components/nav/styles.js
+++ src/app/components/nav/styles.js
@@ -1,38 +1,80 @@
 import styled from "styled-components";
 
 export const Container = styled.nav `
-    width: 100%;
+    width:100vw;
     height: 116px;
     display: flex;
     justify-content: center;
     align-items: center;
+    border-bottom: 1px solid #3E3B47;
 
     #itensNav {
+        width: 100%;
         display: flex;
         align-items: center;
+        justify-content: center;
+        gap: 64px;
 
         #divLogo {
             width: 164px;
             height: 32px;
         
-        span {
-            font-family: 'Roboto Slab';
-            font-style: normal;
-            font-weight: 700;
-            font-size: 24px;
-            line-height: 32px;
-            color: #FF859B;
-         }
+            span {
+                font-family: 'Roboto Slab';
+                font-style: normal;
+                font-weight: 700;
+                font-size: 24px;
+                line-height: 32px;
+                color: #FF859B;
+            }
         }
 
-        #divBusca {
+        #divSearch {
             margin: auto 0;
+            width: 630px;
 
             input {
                 background-color: #262529;
+                width: 100%;
                 height: 56px;
                 border: none;
                 border-radius: 10px;
+            }
+        }
+
+        #divProfile {
+            display: flex;
+            gap: 9px;
+
+            #profileButtons {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: flex-end;
+                
+
+                a {
+                    text-decoration: none;
+                }
+
+                .username {
+                    font-weight: 700;
+                    font-size: 14px;
+                    line-height: 18px;
+                    color: #000000;
+                }
+
+                .exitpage {
+                    color: #948F99;
+                }
+
+            }
+
+            #imgProfile {
+                width: 64px;
+                height: 64px;
+                border-radius: 100px;
+                border: 1px solid #3E3B47;
             }
         }
     }
